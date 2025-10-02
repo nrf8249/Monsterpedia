94f6f55aa3cd03b137f98e26fb4a6a532e830d3b
Xing Su <143617128+xsu217822@users.noreply.github.com>
2025-10-02 10:59:26 -0400

    Add walls and colliders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,28 +4,36 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
-    public float moveSpeed = 5f;
     private Vector2 movement;
+
+    public float moveSpeed = 5f;
+    public string barrierTag = "barrierd";
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        rb.gravityScale = 0;              // 2D 顶视角通常要关重力
+        rb.freezeRotation = true;         // 不要因碰撞旋转
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movement * moveSpeed;
+        Vector2 newPos = rb.position + movement * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPos);
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(barrierTag))
+        {
+            Debug.Log("Hit barrier, movement blocked.");
+        }
     }
 }
