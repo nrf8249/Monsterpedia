07f34946a732575dc0d236a53da112b274cc9bfa
nrf8249 <nrf8249@rit.edu>
2025-10-02 22:50:55 -0400

    started accusation -- gonna have to be a next sprint problem ;0

---
+++ Assets/Scripts/AccusationManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AccusationManager : MonoBehaviour
+{
+    public static AccusationManager Instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartAccusation(NPC npc)
+    {
+
+    }
+}
