9480a7a8ae604441e4b35d56cece14551f8729ee
xsu217822 <suxing412@126.com>
2025-10-08 14:49:58 -0400

    move something

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum GameState
+    {
+        MainMenu,
+        FreeMove,
+        InDialogue,
+        Paused,
+        GameOver
+    }  
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
