7aa0f2e75358b8a8a83e4f21fdbadcb2634195d5
nrf8249 <nrf8249@rit.edu>
2025-10-30 23:34:40 -0400

    added barriers and fixed collisions

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,9 +5,9 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Animator animator;
 
     public float moveSpeed = 5f;
-    public string barrierTag = "barrierd";
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;              // 2D 顶视角通常要关重力
         rb.freezeRotation = true;         // 不要因碰撞旋转
+
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -26,12 +28,30 @@ public class Player : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", movement.x);
+            animator.SetFloat("LastInputY", movement.y);
+        }
+
         movement = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", movement.x);
+        animator.SetFloat("InputY", movement.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            Debug.Log("Collected an item!");
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(barrierTag))
+        if (collision.gameObject.CompareTag("Barrier"))
         {
             Debug.Log("Hit barrier, movement blocked.");
         }
